a3d47230f0e0aa3a481f88c439305848911d6877
harrisonCassar - Laptop <harrison.cassar@gmail.com>
2019-05-13 11:49:49 -0700

    Completed modifications for update function taking a string to be parsed and a new value. Started component separation of tables in the left and right columns ("Live" vs "Overall" tables)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React, {Component} from 'react';
 import Table from './Table';
 
+var tableNumbers = {
+    "B1": 0,
+    "B2": 1,
+    "Pod": 2,
+};
+
+var rowNumbers = {
+    "Voltage": 0,
+    "Current": 1,
+    "Temp": 2,
+    "M1": 0,
+    "M2": 1,
+    "M3": 2,
+    "M4": 3,
+};
+
+var columnNumbers = {
+    "Min": 0,
+    "Max": 1,
+    "Avg": 2,
+};
+
+
 //tableNum and rowNum starts at index 0
-function updateValue(tableNum,rowNum,newValue)
+function updateValue(tableString,newValue)
 {
-    window.appComponent.updateCell(tableNum,rowNum,newValue);
+    window.appComponent.updateCell(tableString,newValue);
 }
 
 class App extends Component {
@@ -15,7 +38,7 @@ class App extends Component {
 
             //each index of tables array indicates different table; can have mapping function
             //array indexes correspond to data's row numbers from 0 to n-1 rows
-            tables: [
+            livetables: [
                 {
                     tableName: "Battery",
                     tableLabel: "1",
@@ -47,20 +70,111 @@ class App extends Component {
                     bgColors: Array(5).fill('aqua')
                 }
             ],
+
+            overalltables: [
+                {
+                    tableName: "Battery",
+                    tableLabel: "1",
+                    labels: ['Min','Current','Temperature'],
+                    rows: 3,
+                    values: [
+                        Array(3).fill(-1),
+                        Array(3).fill(-1),
+                        Array(3).fill(-1),
+                    ],
+                    bgColors: Array(3).fill('white'),
+                },
+                {
+                    tableName: "Battery",
+                    tableLabel: "2",
+                    labels: ['Voltage','Current','Temperature'],
+                    rows: 3,
+                    values: [
+                        Array(3).fill(-1),
+                        Array(3).fill(-1),
+                        Array(3).fill(-1),
+                    ],
+                    bgColors: Array(3).fill('white'),
+                },
+                {
+                    tableName: "Pod",
+                    tableLabel: "",
+                    labels: ['Temperature','Motor 1 Current','Motor 2 Current','Motor 3 Current','Motor 4 Current'],
+                    rows: 5,
+                    values: [
+                        Array(5).fill(-1),
+                        Array(5).fill(-1),
+                        Array(5).fill(-1),
+                    ],
+                    bgColors: Array(5).fill('white'),
+                }
+            ],
         };
     }
 
-    updateCell(tableNum,rowNum,newValue)
+    updateCell(tableString,newValue)
     {
         const updatedState = this.state;
 
         let newColor = 'lightgreen';
+        let tableNum;
+        let rowNum;
+        let modifyLive;
+        let columnNum;
+        let parsedTableString = tableString.split("_");
 
-        if (newValue < this.state.tables[tableNum].minValues[rowNum] || newValue > this.state.tables[tableNum].maxValues[rowNum])
-            newColor = '#ff4d4d';
+        if (parsedTableString.length() != 3)
+        {
+            console.log("Error: table string for update function is invalid.")
+            return;
+        }
 
-        updatedState.tables[tableNum].values[rowNum] = newValue;
-        updatedState.tables[tableNum].bgColors[rowNum] = newColor;
+        //properly set tableNum index
+        if (!(parsedTableString[0] in tableNumbers))
+        {
+            console.log("Error: table string for update function is invalid.")
+            return;
+        }
+        else
+            tableNum = tableNumbers[parsedTableString[0]];
+
+        //properly set rowNum
+        if (!(parsedTableString[1] in rowNumbers))
+        {
+            console.log("Error: table string for update function is invalid.")
+            return;
+        }
+        else
+            rowNum = tableNumbers[parsedTableString[1]];
+
+        //properly set columnNum
+        if (parsedTableString[2] == "Actual")
+            modifyLive = true;
+        else
+        {
+            modifyLive = false;
+            if (!(parsedTableString[2] in columnNumbers))
+            {
+                console.log("Error: table string for update function is invalid.")
+                return;
+            }
+            else
+                columnNum = columnNumbers[parsedTableString[2]];
+        }
+
+        //update value and bgColor in state object
+        if (modifyLive)
+        {
+            if (newValue < this.state.livetables[tableNum].minValues[rowNum] || newValue > this.state.tables[tableNum].maxValues[rowNum])
+                newColor = '#ff4d4d';
+
+            updatedState.livetables[tableNum].values[rowNum] = newValue;
+            updatedState.livetables[tableNum].bgColors[rowNum] = newColor;
+        }
+        else
+        {
+            updatedState.overalltables[tableNum].values[columnNum][rowNum] = newValue;
+        }
 
         this.setState(updatedState);
         
@@ -86,46 +200,46 @@ class App extends Component {
                 </div>
 
                 <table class="parent">
-                    <div class="leftchild">
+                    <div class="leftcolumn">
                         <Table
                         tableLabel="1"
-                        labels={this.state.tables[0].labels}
-                        minValues={this.state.tables[0].minValues}
-                        maxValues={this.state.tables[0].maxValues}
-                        values={this.state.tables[0].values}
-                        bgColors={this.state.tables[0].bgColors}
+                        labels={this.state.livetables[0].labels}
+                        minValues={this.state.livetables[0].minValues}
+                        maxValues={this.state.livetables[0].maxValues}
+                        values={this.state.livetables[0].values}
+                        bgColors={this.state.livetables[0].bgColors}
                         rows={3}
                         />
 
                         <Table
                         tableLabel="2"
-                        labels={this.state.tables[1].labels}
-                        minValues={this.state.tables[1].minValues}
-                        maxValues={this.state.tables[1].maxValues}
-                        values={this.state.tables[1].values}
-                        bgColors={this.state.tables[1].bgColors}
+                        labels={this.state.livetables[1].labels}
+                        minValues={this.state.livetables[1].minValues}
+                        maxValues={this.state.livetables[1].maxValues}
+                        values={this.state.livetables[1].values}
+                        bgColors={this.state.livetables[1].bgColors}
                         rows={3}
                         />
 
                     </div>
-                    <div class="rightchild">
+                    <div class="rightcolumn">
                         <Table
                         tableLabel="1"
-                        labels={this.state.tables[0].labels}
-                        minValues={this.state.tables[0].minValues}
-                        maxValues={this.state.tables[0].maxValues}
-                        values={this.state.tables[0].values}
-                        bgColors={this.state.tables[0].bgColors}
+                        labels={this.state.overalltables[0].labels}
+                        minValues={this.state.overalltables[0].values[0]}
+                        maxValues={this.state.overalltables[0].values[1]}
+                        values={this.state.overalltables[0].values[2]}
+                        bgColors={this.state.overalltables[0].bgColors}
                         rows={3}
                         />
 
                         <Table
                         tableLabel="2"
-                        labels={this.state.tables[1].labels}
-                        minValues={this.state.tables[1].minValues}
-                        maxValues={this.state.tables[1].maxValues}
-                        values={this.state.tables[1].values}
-                        bgColors={this.state.tables[1].bgColors}
+                        labels={this.state.overalltables[1].labels}
+                        minValues={this.state.overalltables[1].values[0]}
+                        maxValues={this.state.overalltables[1].values[1]}
+                        values={this.state.overalltables[1].values[2]}
+                        bgColors={this.state.overalltables[1].bgColors}
                         rows={3}
                         />
                     </div>
@@ -138,23 +252,23 @@ class App extends Component {
                 </div>
 
                 <table class="parent">
-                    <div class="leftchild">
+                    <div class="leftcolumn">
                         <Table
-                            labels={this.state.tables[2].labels}
-                            minValues={this.state.tables[2].minValues}
-                            maxValues={this.state.tables[2].maxValues}
-                            values={this.state.tables[2].values}
-                            bgColors={this.state.tables[2].bgColors}
+                            labels={this.state.livetables[2].labels}
+                            minValues={this.state.livetables[2].minValues}
+                            maxValues={this.state.livetables[2].maxValues}
+                            values={this.state.livetables[2].values}
+                            bgColors={this.state.livetables[2].bgColors}
                             rows={5}
                         />
                     </div>
-                    <div class="rightchild">
+                    <div class="rightcolumn">
                         <Table
-                            labels={this.state.tables[2].labels}
-                            minValues={this.state.tables[2].minValues}
-                            maxValues={this.state.tables[2].maxValues}
-                            values={this.state.tables[2].values}
-                            bgColors={this.state.tables[2].bgColors}
+                            labels={this.state.overalltables[2].labels}
+                            minValues={this.state.overalltables[2].values[0]}
+                            maxValues={this.state.overalltables[2].values[1]}
+                            values={this.state.overalltables[2].values[2]}
+                            bgColors={this.state.overalltables[2].bgColors}
                             rows={5}
                         />
                     </div>
--- src/Table.js
+++ src/Table.js
@@ -1,6 +1,18 @@
 import React, {Component} from 'react';
 import {LabelCell, StaticCell, DynamicCell} from './Cell';
 
+/*
+    TODO:
+    Need to separate Live and Overall Tables into two separate components.
+    Implementations for the Overall Table has been started, basing it off of
+    the implementation for the Live Tables. NEED to make sure the values being
+    passed through components are being done properly. Shouldn't be passing in
+    "minValues" or "maxValues", but instead be passing in one 2D "values" array.
+    This will make rendering them easier (can use a for loop with a general
+    "DynamicCell" component like we had before for the min/max Static Cells
+    for the Live Tables.)
+*/
+
 class Table extends Component {
     constructor(props) {
         super(props);
@@ -61,6 +73,98 @@ class Table extends Component {
                     />
                 </table>
             </div>
+        );
+    }
+}
+
+class OverallTable extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    renderDynamicCell(i,j) {
+        return (
+            <DynamicCell
+                value={this.props.values[j][i]}
+                bgColor={this.props.bgColors[i]}
+            />
+        );
+    }
+
+    renderLabelCell(i) {
+        return (
+            <LabelCell
+                label={this.props.labels[i]}
+            />
+        );
+    }
+
+    render() {
+        //wait for passed in custom min/max values or labels, will override (if none passed in, don't render table)
+        if (!this.props.minValues || !this.props.maxValues || !this.props.labels)
+            return null;
+
+        return (
+            <div>
+                <br />
+                <table class="customtable">
+                    <OverallTableHeader />
+
+                    <OverallTableBody
+                        rows={this.props.rows}
+                        renderLabelCell={(i) => this.renderLabelCell(i)}
+                        renderStaticCell={(i,type) => this.renderStaticCell(i,type)}
+                        renderDynamicCell={(i,j) => this.renderDynamicCell(i,j)}
+                    />
+                </table>
+            </div>
+        );
+    }
+}
+
+class OverallTableHeader extends Component {
+    render() {
+        return (
+            <thead>
+                <tr>
+                    <th className="othercol" bgcolor={'white'}>Min</th>
+                    <th className="othercol" bgcolor={'white'}>Actual</th>
+                    <th className="othercol" bgcolor={'white'}>Max</th>
+                </tr>
+            </thead>
+        );
+    }
+}
+
+class OverallTableBody extends Component {
+    
+    renderRow(i)
+    {
+        let row = [];
+
+        
+        for (var j = 0; j < 3; j++)
+            row.push(this.props.renderDynamicCell(i,j));
+
+        return (
+            <tr>
+                {row}
+            </tr>
+        );
+    }
+
+    render() {
+
+        let rows = [];
+
+        for (var i = 0; i < this.props.rows; i++) {
+            rows.push(this.renderRow(i));
+        }
+
+        return (
+            <tbody>
+                {rows}
+            </tbody>
         );
     }
 }
